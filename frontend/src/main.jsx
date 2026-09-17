41c86017996737a8dcb373650c9065c646cf7496
Swapnil9637 <shelkenarayan118@gmail.com>
2026-09-17 17:50:39 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async'
+import { TracePilotProvider } from '@swapnil454/tracepilot/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HelmetProvider>
-      <App />
+      <TracePilotProvider 
+        token="YOUR_PROJECT_TRACKING_ID" 
+        serviceName="FeedBack-Frontend"
+        ingestorUrl="https://deployraai.onrender.com/api/observability/traces/v1/traces"
+        rumUrl="https://deployraai.onrender.com/api/observability/traces/v1/rum"
+        enableSessionReplay={true}
+      >
+        <App />
+      </TracePilotProvider>
     </HelmetProvider>
   </StrictMode>,
 )
