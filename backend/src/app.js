9b7d9c7a3a9393b5accacb6ea426e9f5f51e9d2e
Swapnil9637 <shelkenarayan118@gmail.com>
2026-09-20 00:29:46 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -126,6 +126,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'traceparent', 'tracestate'],
 }));
 
 const limiter = rateLimit({
